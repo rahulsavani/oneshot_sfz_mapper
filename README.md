892f8ccdda3a8e85abd284ef6de1b90fbd200db9
Rahul Savani <rahul.savani@gmail.com>
2020-05-17 12:18:20 +0100

    populated README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # oneshot_sfz_mapper
+
+Creates an SFZ file (see https://sfzformat.com/tutorials/drum_basics) that maps
+all one shots from a given directoy across the keyboard (a la
+http://mildon.me/beagle.php?b=view&id=UVIU)
+
+The function process_one_dir takes as input a path to one directory of one shots
+it creates an SFZ file that maps the one shots across the keyboard to adjacent
+keys
+
+__main__ runs process_one_dir for every subdirectory in a root directory for
+batch processing
+
--- mapper.py
+++ mapper.py
@@ -1,12 +1,17 @@
 """
-Purpose: create an SFZ file (see https://sfzformat.com/tutorials/drum_basics) 
-that maps one shots across the keyboard (a la http://mildon.me/beagle.php?b=view&id=UVIU)
+Author: rahul.savani@gmail.com
 
-process_one_dir takes as input a path to one directory of one shots it creates
-an SFZ file that maps the one shots across the keyboard to adjacent keys
+Creates an SFZ file (see https://sfzformat.com/tutorials/drum_basics) that maps
+all one shots from a given directoy across the keyboard (a la
+http://mildon.me/beagle.php?b=view&id=UVIU)
 
-__main__: runs process_one_dir for every subdirectory in a root directory for
+The function process_one_dir takes as input a path to one directory of one shots
+it creates an SFZ file that maps the one shots across the keyboard to adjacent
+keys
+
+__main__ runs process_one_dir for every subdirectory in a root directory for
 batch processing
+
 """
 
 import os
